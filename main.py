dc8bf4c98f9854be56d85347d68eb19d376e3da5
Razeen Mohamed Rasheed <razeenrasheed83@gmail.com>
2024-10-12 19:36:47 +0530

    Added cors

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from src.routes import taskroutes,usermanagement
+from fastapi.middleware.cors import CORSMiddleware
 import uvicorn
 
 app = FastAPI()
@@ -12,6 +13,21 @@ async def welcome():
 
 app.include_router(taskroutes.router)
 app.include_router(usermanagement.router)
+
+origins = [
+    "http://localhost.tiangolo.com",
+    "https://localhost.tiangolo.com",
+    "http://localhost",
+    "http://localhost:8080",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
